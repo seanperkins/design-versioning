6501ea89ccc2d5336bd819ac461c7be2587055c7
Sean Perkins <sean@mobility-labs.com>
2015-09-19 15:18:57 -0400

    Version numbers work

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # design-versioning
+
+Update the revision.js to contain the correct directories for watching and the destination directory
+
+Currently just saves all files to a directory with their name.
--- index.js
+++ index.js
@@ -1,31 +1,28 @@
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
-
-var util     = require('util'),
-    chokidar = require('chokidar'),
-    _        = require('lodash'),
-    colors   = require('colors'),
-    fse      = require('fs-extra'),
-    revision = require('./revision');
-
-var files = [],
-    watchDirectory = revision.watchDir,
-    destinationDirectory = revision.destinationDir;
+var util     = require('util');
+var chokidar = require('chokidar');
+var _        = require('lodash');
+var colors   = require('colors');
+var fse      = require('fs-extra');
+var revision = require('./revision.json');
 
 // One-liner for current directory, ignores .dotfiles
-var watcher = chokidar.watch(watchDirectory, {ignored: /[\/\\]\./})
+var destinationDirectory = revision.destinationDir;
+var watchDirectory = revision.watchDir;
+var watcher = chokidar.watch(watchDirectory, {ignored: /[\/\\]\./});
 
 watcher.on('ready', function(){
-  watcher.on('change', function(path, event) {
+  watcher.on('change', function fileChanged(path, event) {
     stageFile(path);
   })
-  watcher.on('add', function(path, event) {
+  watcher.on('add', function fileAdded(path, event) {
     stageFile(path);  
   })
-  watcher.on('remove', function(path, event) {
+  watcher.on('remove', function fileRemoved(path, event) {
     removeFile(path); 
   });
-})
+});
+
+var files     = [];
 
 stageFile = function(path) {
   if (!_.includes(files, path)) {
@@ -45,11 +42,25 @@ removeFile = function(path) {
   }
 }
 
-process.stdin.on('data', function (text) {
-  console.log('Your message:', text);
-  if (isValid(text)) {
-    createVersion(text);
+commitPrompt = function(newFiles) {
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+  if (newFiles === files) {
+    console.log('Staged Files',files);
   }
+  process.stdout.write('<Increment>'.red + ' <Commit Message>'.green);
+}
+
+//Listen to command line prompt for message
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function receiveInput(text) {
+  
+  if (typeof parseMessage(text) === 'object') {
+    var parsedMessage = parseMessage(text);
+    createVersion(parsedMessage);
+  }
+  
   if (text === 'quit\n') {
     done();
   }
@@ -60,27 +71,29 @@ function done() {
   process.exit();
 }
 
-commitPrompt = function(newFiles) {
-  process.stdout.clearLine();
-  process.stdout.cursorTo(0);
-  if (newFiles === files) {
-    console.log('Staged Files',files);
-  }
-  process.stdout.write('<Increment>'.red + ' <Commit Message>'.green);
-}
-
-isValid = function() {
-  return true;
+parseMessage = function(text) {
+  var match = text.match(/^(\+*)?\s?(.*)/);
+  //Increment should be a minimum of 1 to ensure we are advancing the version
+  var increment = typeof match[1] !== 'undefined'
+                ? Math.max(match[1].length, 1)
+                : 1;
+  return {
+    increment: increment,
+    message: match[2]
+  };
 }
 
 var fileRegEx = new RegExp('('+watchDirectory+')(.*)');
+var versionArray = [0,0,0];
 
-createVersion = function(text) {
-
+createVersion = function(parsedObject) {
+  var message = parsedObject.message;
+  var increment = parsedObject.increment;
+  var newVersion = getNewVersionString(increment, versionArray);
+  
   _.each(files, function(file){
     var match = file.match(fileRegEx);
     var fileName = match[2];
-    var message = text.replace(/\n/,'');
 
     fse.mkdir(destinationDirectory + fileName, function(err){
       if(err){
@@ -88,11 +101,44 @@ createVersion = function(text) {
       }
     });
     
-    fse.copy(file, destinationDirectory+fileName+'/' + message, function(err){
+    fse.copy(file, destinationDirectory+fileName+'/' + newVersion + ' ' + message, function(err){
       if(err){
         console.log(err)
       }
     });
-  })
+  });
+
   files = [];
+}
+
+versionStringToArray = function(versionString) {
+  if (typeof versionString === 'string') {
+    var versionArray = versionString.split('.')
+    versionArray.map(function(a){
+      return parseInt(a, 10);
+    });
+    return versionArray;
+  }
+}
+
+versionArrayToString = function(versionArray) {
+  return versionArray.join('.');
+}
+
+incrementVersionNumber = function(incrementIndex, versionArray) {
+  var adjustedIndex = 2 - incrementIndex;
+  for (var i = adjustedIndex; i <= 2; i++) {
+    if (i === adjustedIndex) {
+      versionArray[i] += 1;
+    }
+    if (i > adjustedIndex) {
+      versionArray[i] = 0;
+    }
+  };
+  return versionArray;
+}
+
+getNewVersionString = function(increment, versionArray) {
+  var versionArray = incrementVersionNumber(increment, versionArray)
+  return versionArrayToString(versionArray);
 }
